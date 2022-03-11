5ea526b69cacf18a611224753496b7865424fb3a
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 <1856059@01-19961.pusd.dom>
2022-03-11 08:44:14 -0800

    finally

---
+++ p1Game/src/p1Game/Queue.java
@@ -0,0 +1,81 @@
+package p1Game;
+
+import java.util.Stack;
+
+//For this problem, you may assume you can use the Stack we started together in class
+public class Queue<T>{
+
+  //instance vars to be added by student
+  
+  private Stack<T> in = new Stack<T>();
+  private Stack<T> out = new Stack<T>();
+	private int size = 0;
+
+  //constructor to be added by student
+  
+  /*
+  enQueue(q, x): 
+  
+  While stack1 is not empty, push everything from stack1 to stack2.
+  Push x to stack1 (assuming size of stacks is unlimited).
+  Push everything back to stack1.
+
+  deQueue(q): 
+
+  If stack1 is empty then error
+  Pop an item from stack1 and return it*/
+  
+  //remove and return the element at the beginning of the list
+  public T remove(){
+      if (out.size() == 0)
+      {
+          return null;
+      }
+      return out.pop();
+      /*if (out.size() == 0 && in.size() == 0) {
+          return null;
+      }
+      if (out.size() > 0) {
+          return out.pop();
+      } else if (out.size() == 0) {
+          while (in.size() > 0) {
+              out.push(in.pop());
+          }
+          return out.pop();
+      }*/
+  }
+
+
+  //add element at the end of the list
+  public void add(T el){
+      while (out.size() != 0) {
+          in.push(out.pop());
+      }
+      out.push(el);
+      while (in.size() != 0) {
+          out.push(in.pop());
+      }
+      /*if (out.size() == 0) {
+          in.push(el);
+          size++;
+      } else {
+          while (out.size() > 0) {
+              in.push(out.pop());
+              size++;
+          }
+          in.push(el);
+          size++;
+      }*/
+  }
+
+  //return a String representation of the list
+  //formatted as [el1, el2, el3, ..., elN]
+  public String toString(){
+      return "[" + in + "]";  
+  }
+
+  public int size(){
+    return out.size();
+  }
+
+}
---
+++ p1Game/src/p1Game/p1.java
@@ -0,0 +1,199 @@
+package p1Game;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class p1 {
+	
+	private static Queue<Integer> enqueue = new Queue<Integer>();
+	private static Queue<Integer> enqueue2 = new Queue<Integer>();
+	private static Queue<Character> enqueueChar = new Queue<Character>();
+	private static Queue<Integer> dequeue = new Queue<Integer>();
+	private static Queue<Integer> dequeue2 = new Queue<Integer>();
+	private static Queue<Character> dequeueChar = new Queue<Character>();
+	private static int rows = 0;
+	private static int cols = 0;
+	private static int rooms = 0;
+	private static boolean cake = false;
+	private static boolean full = false;
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner scanner;
+		File f = new File("map1.txt");
+		
+
+			//code that might throw a special error
+			scanner = new Scanner(f);
+			
+			//use next methods to grab the first
+			//3 numbers from the file for your
+			//map info
+			
+			rows = scanner.nextInt();
+			cols = scanner.nextInt();
+			rooms = scanner.nextInt();
+			
+			char[][] coordinate = new char [rows * rooms][cols];
+			
+			if (f.getName().contains("Coordinate")) { 
+				coordinateBased(scanner);
+			} else {
+				//take in the # of columns and rows
+				//scanner.nextLine();
+				
+				int count = -1;
+				while(scanner.hasNextLine() && count <= rows * rooms) {
+					String line = scanner.nextLine();
+					
+					//use charAt to grab the elements of the map
+					//for a given row
+					
+					for (int i = 0; i < line.length(); i++) {
+						coordinate[count][i] = line.charAt(i);
+						System.out.print(line.charAt(i));
+					}
+					System.out.println();
+					count++;
+				}
+				System.out.println();
+			}
+			
+		int startX = 0;
+		int startY = 0;
+		
+		for (int i = 0; i < coordinate.length; i++) {
+			for (int j = 0; j < coordinate[0].length; j++) {
+				if (coordinate[i][j] == 'K') {
+					enqueue.add(i);
+					enqueue2.add(j);
+					enqueueChar.add('K');
+					startX = i;
+					startY = j;
+				}
+			}
+		}
+		
+		boolean[][] visit = new boolean [rows * rooms][cols];
+		
+		//for () {
+		int num = 0;
+			while (cake == false && full == false) {
+				dequeue.add(enqueue.remove());
+				dequeue2.add(enqueue2.remove());
+				dequeueChar.add(enqueueChar.remove());
+					if (!(startX - 1 < 0 || coordinate[startX - 1][startY] == '@' || visit[startX - 1][startY] == true)) {
+						enqueue.add(startX - 1);
+						enqueue2.add(startY);
+						enqueueChar.add(coordinate[startX - 1][startY]);
+						if (coordinate[startX - 1][startY] == 'C') {
+							cake = true;
+						}
+						if (coordinate[startX - 1][startY] == 'C') {
+							visit[startX - 1][startY] = true;
+						}
+						System.out.println(num++);
+					}
+					if (!(startX + 1 >= coordinate.length || coordinate[startX + 1][startY] == '@' || visit[startX + 1][startY] == true)) {
+						enqueue.add(startX + 1);
+						enqueue2.add(startY);
+						enqueueChar.add(coordinate[startX + 1][startY]);
+						if (coordinate[startX + 1][startY] == 'C') {
+							cake = true;
+						}
+						if (coordinate[startX + 1][startY] == 'C') {
+							visit[startX + 1][startY] = true;
+						}
+						System.out.println(num++);
+					}
+					if (!(startY + 1 >= coordinate[0].length || coordinate[startX][startY + 1] == '@' || visit[startX][startY + 1] == true)) {
+						enqueue.add(startX);
+						enqueue2.add(startY + 1);
+						enqueueChar.add(coordinate[startX][startY + 1]);
+						if (coordinate[startX][startY + 1] == 'C') {
+							cake = true;
+						}
+						if (coordinate[startX][startY + 1] == 'C') {
+							visit[startX][startY + 1] = true;
+						}
+						System.out.println(num++);
+					}
+					if (!(startY - 1 < 0 || coordinate[startX][startY - 1] == '@' || visit[startX][startY - 1] == true)) {
+						enqueue.add(startX);
+						enqueue2.add(startY - 1);
+						enqueueChar.add(coordinate[startX][startY - 1]);
+						if (coordinate[startX][startY - 1] == 'C') {
+							cake = true;
+						}
+						if (coordinate[startX][startY - 1] == 'C') {
+							visit[startX][startY - 1] = true;
+						}
+						System.out.println(num++);
+					}
+				}
+				
+				System.out.println("found it");
+			}
+	//}
+	
+	public static void coordinateBased(Scanner scan) {
+		
+			char[][] coordinate = new char[rows * rooms][cols];
+			
+			for (int k = 0; k < rows * rooms; k++) {
+				for (int j = 0; j < cols; j++) {
+					coordinate[k][j] = '.';
+				}
+			}
+			
+			char[] item = new char[rows * cols * rooms];
+			int[] row = new int[rows * cols * rooms];
+			int changeRoom = 0;
+			int count = -1;
+			int[] col = new int[rows * cols * rooms];
+			
+			while(scan.hasNextLine()) {
+				
+				String line = scan.nextLine();
+				//use charAt to grab the elements of the map
+				//for a given row
+				
+				count++;
+				
+				//fix for numbers greater than 1 digit
+				for (int j = 0; j < line.length(); j++) {
+					if (j == 0) {
+						char Character = line.charAt(j);
+						item[count] = Character;
+					} else if (j == 2) {
+						char Character = line.charAt(j);
+						row[count] = Character - 48;
+						if (row[count] < row[count - 1] && count >= 1) {
+							changeRoom++;
+						}
+					} else if (j == 4) {
+						char Character = line.charAt(j);
+						col[count] = Character - 48;
+					}
+				}
+				
+				if (changeRoom == 0) {
+					coordinate[row[count]][col[count]] = item[count];
+				} else {
+					coordinate[row[count] + rows * changeRoom][col[count]] = item[count];
+				}
+			}
+			
+			coordinate[0][0] = '.';
+			
+			for (int k = 0; k < rows * rooms; k++) {
+				for (int j = 0; j < cols; j++) {
+					System.out.print(coordinate[k][j]);
+				}
+				System.out.println();
+			}
+		
+	}
+
+}
